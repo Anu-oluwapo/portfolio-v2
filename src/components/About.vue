a9fe692795c133df4661724ff170767bb9fac74e
Anuoluwapo Abolarin <AAbolarin@CHIPLC.COM>
2023-03-16 16:12:21 +0100

    Minor chnges

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,12 +1,12 @@
 <template>
     <section class="about-section">
         <div class="section-header">
+            <div class="content">
             <div><h1><span>ABOUT</span></h1> <Arrow class="arrow" color="#242424" /></div>
             <div><h1>ABOUT</h1> <Arrow class="arrow" color="white" /></div>
             <div><h1><span>ABOUT</span></h1> <Arrow class="arrow" color="#242424" /></div>
-            <div><h1>ABOUT</h1> <Arrow class="arrow" color="white" /></div>
-            
-            
+            <!-- <div><h1>ABOUT</h1> <Arrow class="arrow" color="white" /></div> -->
+        </div>
         </div>
           <div class="container d-flex">
               
