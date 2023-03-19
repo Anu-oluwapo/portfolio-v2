6160edace44150e71e42af49d924bb7392af1f3f
Anuoluwapo Abolarin <AAbolarin@CHIPLC.COM>
2023-03-19 23:11:33 +0100

    Performance Updates

--- src/components/icons/Switch.vue
+++ src/components/icons/Switch.vue
@@ -1,51 +1,49 @@
 <template>
-  <div @click="active=!active" class="switch-icon">
-    <div class="sun"><img src="@/assets/sun.svg"/></div>
-    <div class="moon"><img src="@/assets/moon.svg"/></div>
-    <div class="ball" :class="[active&&'active']"></div>
+  <div @click="active = !active" class="switch-icon">
+    <div class="sun"><img alt="sun-icon" src="@/assets/sun.svg" /></div>
+    <div class="moon"><img alt="moon-icon" src="@/assets/moon.svg" /></div>
+    <div class="ball" :class="[active && 'active']"></div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-let active = ref(true)
+import { ref } from "vue";
+let active = ref(true);
 </script>
 
-
 <style lang="scss" scoped>
-@import '@/assets/variables.scss';
+@import "@/assets/variables.scss";
 
-.switch-icon{
-    border: 1.5px solid $neutral;
-    border-radius: 100px;
+.switch-icon {
+  border: 1.5px solid $neutral;
+  border-radius: 100px;
+  display: flex;
+
+  div {
+    margin: 5px;
     display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 3rem;
+    width: 3rem;
+    cursor: pointer;
 
-    div{
-        margin: 5px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 3rem;
-        width: 3rem;
-        cursor: pointer;
-        
-        img{
-            justify-content: center;
-        }
+    img {
+      justify-content: center;
     }
+  }
 
-    .ball{
-      background: $white;
-      border-radius: 100px;
-      align-items: center;
-      cursor: default;
-      transition: all 0.3s ease;
-      position: absolute;
-      z-index: -1;
-    }
-    .active{ 
-      transform: translateX(40px);
-    }
+  .ball {
+    background: $white;
+    border-radius: 100px;
+    align-items: center;
+    cursor: default;
+    transition: all 0.3s ease;
+    position: absolute;
+    z-index: -1;
+  }
+  .active {
+    transform: translateX(40px);
+  }
 }
-
 </style>
