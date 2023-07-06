cf5e688d55d5976a6dc1a6408c48d25e19587bda
Anuoluwapo Abolarin <AAbolarin@CHIPLC.COM>
2023-07-06 14:04:35 +0100

    Added New  Project

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -3,6 +3,32 @@
     <SectionHeader :text="'WORK'" />
     <div class="">
       <div class="projects">
+        <div
+          @click="openTab('https://og-clothier.netlify.app/')"
+          class="project"
+        >
+          <div class="container">
+            <div class="dot"></div>
+            <h3>OG-Clothiers</h3>
+          </div>
+          <div class="container">
+            <div class="skills">
+              <span style="--i: 0">HTML</span>
+              <span style="--i: 1">CSS</span>
+              <span style="--i: 2">JAVASCRIPT</span>
+              <span style="--i: 3">GSAP</span>
+            </div>
+          </div>
+          <div class="image">
+            <a
+              name="project-ogClothiers"
+              target="_blank"
+              href="https://vr-classroom.netlify.app"
+              ><img alt="Project OG Clothiers" src="/images/OG-Clothiers.png" />
+            </a>
+          </div>
+        </div>
+
         <div @click="openTab('https://zenpad.netlify.app')" class="project">
           <div class="container">
             <div class="dot"></div>
@@ -24,6 +50,30 @@
               <img alt="Project Zenpad" src="/images/zenpad.png" />
             </a>
             <div class="cursor"><p>View Site</p></div>
+          </div>
+        </div>
+
+        <div @click="openTab('https://anuoluwapo.xyz')" class="project">
+          <div class="container">
+            <div class="dot"></div>
+            <h3>Portfolio ('23)</h3>
+          </div>
+          <div class="container">
+            <div class="skills">
+              <span style="--i: 0">HTML</span>
+              <span style="--i: 1">SCSS</span>
+              <span style="--i: 3">VUE JS</span>
+              <span style="--i: 4">GSAP</span>
+              <!-- <span style="--i: 5">LENIS</span> -->
+            </div>
+          </div>
+          <div style="background-color: #131313" class="image">
+            <a
+              name="project-portfolio"
+              target="_blank"
+              href="https://anuoluwapo.xyz"
+              ><img alt="Project Portfolio" src="/images/portfolio.png"
+            /></a>
           </div>
         </div>
         <div
@@ -49,29 +99,6 @@
               href="https://vr-classroom.netlify.app"
               ><img alt="Project VR Classroom" src="/images/classroom.png" />
             </a>
-          </div>
-        </div>
-        <div @click="openTab('https://anuoluwapo.xyz')" class="project">
-          <div class="container">
-            <div class="dot"></div>
-            <h3>Portfolio ('23)</h3>
-          </div>
-          <div class="container">
-            <div class="skills">
-              <span style="--i: 0">HTML</span>
-              <span style="--i: 1">SCSS</span>
-              <span style="--i: 3">VUE JS</span>
-              <span style="--i: 4">GSAP</span>
-              <!-- <span style="--i: 5">LENIS</span> -->
-            </div>
-          </div>
-          <div style="background-color: #131313" class="image">
-            <a
-              name="project-portfolio"
-              target="_blank"
-              href="https://anuoluwapo.xyz"
-              ><img alt="Project Portfolio" src="/images/portfolio.png"
-            /></a>
           </div>
         </div>
 
